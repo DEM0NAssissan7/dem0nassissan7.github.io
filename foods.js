894fc775271053505bb7a0327476ae6ad4214913
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-10-31 11:16:08 -0500

    add pickles and red onions

--- foods.js
+++ foods.js
@@ -9,5 +9,7 @@ add_food("Chicken Breast", 0, 31);
 add_food("Chicken Thighs", 0, 24);
 add_food_amount("Eggs -scrambled-", 1, 6);
 add_food_amount("Hamburger Patties", 0.5, 9);
+add_food("Pickles (by weight)", 2.3, 0);
+add_food("Red Onion (raw)", 5.63, 0);
 add_food("Romaine Lettuce", 3.3, 1.2);
 add_food_amount("Sausage Patty", 1, 9);
