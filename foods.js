2400a5e3c282d231dde941373e402bb96d720958
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-11-12 19:28:18 -0600

    add food

--- foods.js
+++ foods.js
@@ -3,6 +3,7 @@
 Syntax: add_food("Name", g carbs per 100g, g protein per 100g)
 */
 add_food_amount("Avacado", 12, 0);
+add_food("Avacado (by weight)", 8.5, 2);
 add_food("Atlantic Salmon", 0, 20);
 add_food_amount("Beef Bacon -strip-", 0, 3);
 add_food("Beef (short ribs)", 0, 22);
@@ -11,6 +12,7 @@ add_food("Celery", 3, 0);
 add_food("Chicken Breast", 0, 31);
 add_food("Chicken Thigh (boneless)", 0, 24);
 add_food("Chicken Thigh (bone-in)", 0, 18);
+add_food("Chicken Quarter (bone-in)", 0, 12.5);
 add_food("Cucumber", 3.6, 0.7);
 add_food_amount("Eggs -scrambled-", 1, 6);
 add_food_amount("Hamburger Patties", 0.5, 9);
