82128e03a106a8cc2ab3acb572855a2d0e3ac9b0
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-12-21 22:22:26 -0600

    i became more sensitive to insulin

--- profile.js
+++ profile.js
@@ -2,16 +2,16 @@ const profile = {
     e: {
         carbs: 6.2,
         protein: 1.0339,
-        insulin: 35,
-    },
-    p: {
-        carbs: 1.5,
-        protein: 2.3,
-        insulin: 1.75
+        insulin: 50, // Was 35
     },
     n: {
         carbs: 0,
         protein: 1.14,
         insulin: 0.43
+    },
+    p: {
+        carbs: 1.5,
+        protein: 2.3,
+        insulin: 1.75
     }
 }
