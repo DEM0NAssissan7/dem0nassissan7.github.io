6e024f80185e9a3f851b9d85c68c767bfb2d65f4
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-01-30 17:33:37 -0600

    new profile edition

--- profile.js
+++ profile.js
@@ -1,8 +1,8 @@
 const profile = {
     e: {
         carbs: 4.5, // 6.2 -> 5 -> 4.25 -> 4.4 -> 4.5
-        protein: 1.079, // 1.0339 -> 1.02 -> 1.019 -> 1.079
-        insulin: 50.6, // 35 -> 41 -> 48 -> 50 -> 49.8 -> 50.6
+        protein: 0.62, // 1.0339 -> 1.02 -> 1.019 -> 1.079
+        insulin: 35.5, // 35 -> 41 -> 48 -> 50 -> 49.8 -> 50.6
     },
     n: {
         carbs: 0,
@@ -10,8 +10,8 @@ const profile = {
         insulin: 0.43
     },
     p: {
-        carbs: 1.5,
-        protein: 2.3,
-        insulin: 1.75
+        carbs: 1.4,
+        protein: 1.6,
+        insulin: 1.2
     }
 }
