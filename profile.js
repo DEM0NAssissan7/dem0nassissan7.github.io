9e2b7ec42b4ab14dbb7dfbdf41965e66b4b02588
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-03-03 23:49:53 -0600

    reorder profile js file to match webpage layout

--- profile.js
+++ profile.js
@@ -4,15 +4,15 @@ const profile = {
         protein: 0.65, // 0.68 -> 0.65
         insulin: 32, // 36.2 -> 35.4 -> 33.4
     },
+    p: {
+        carbs: 0.83,
+        protein: 1.54,
+        insulin: 1.25,
+    },
     n: {
         carbs: 0.0,
         protein: 0.64,
         insulin: 0.2,
         system: 0, // Do NOT change this value
-    },
-    p: {
-        carbs: 0.83,
-        protein: 1.54,
-        insulin: 1.25,
     }
 }
