d5d4d648d4f836e5c0739dbb06a392b1b58417ea
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-03-09 15:22:05 -0500

    tweak e.insulin back to normal

--- profile.js
+++ profile.js
@@ -2,7 +2,7 @@ const profile = {
     e: {
         carbs: 4.32, // 4.28 -> 4.32
         protein: 0.65, // 0.68 -> 0.65
-        insulin: 32, // 36.2 -> 35.4 -> 33.4
+        insulin: 35.4, // 36.2 -> 35.4 -> 33.4 -> 35.4
     },
     p: {
         carbs: 0.83,
@@ -13,6 +13,6 @@ const profile = {
         carbs: 0.0,
         protein: 0.64,
         insulin: 0.2,
-        system: 0, // Do NOT change this value
+        system: 0, // Do NOT change this value; debug only
     }
 }
